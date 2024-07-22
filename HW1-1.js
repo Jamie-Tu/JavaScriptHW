bc3aa24cf30ceea9576716ff34eabfc25232f75b
Jamie <jamietu08@gmail.com>
2024-07-22 23:45:36 +0800

    HW1第一大題

---
+++ HW1-1.js
@@ -0,0 +1,17 @@
+        // 1.程式基本元素
+        // a. 印出10個你知道的關鍵字
+        console.log("<1a>");
+        console.log('印出10個你知道的關鍵字:var,let,if,else,while,for,const,number,boolean,string')
+
+        // b. 宣告一圓周率常數，值為3.14，並取一個適當的常數名稱
+        
+        console.log("<1b>");
+        const pi=3.14;
+        console.log("pi="+pi);
+
+        // c. 宣告兩個變數: 1.用來存你的名字，2.用來存你的年紀，並印出。 請使用適當的變數名
+        
+        let name="Jamie";
+        let age=30;
+        console.log("<1c>");
+        console.log("My name is " + name + ", I am " + age + " years old.")
